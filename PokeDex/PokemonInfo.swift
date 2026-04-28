09ba87632e638d0d25d88172c56049666a05b52c
BT_C09_16 <mobileappdev@bths.edu>
2026-04-28 13:37:03 -0400

    Added data type

--- PokeDex/PokemonInfo.swift
+++ PokeDex/PokemonInfo.swift
@@ -7,12 +7,7 @@
 
 import SwiftUI
 
-struct PokemonInfo: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    PokemonInfo()
+struct PokemonInfo: Identifiable{
+    let id = UUID()
+    var name: String
 }
