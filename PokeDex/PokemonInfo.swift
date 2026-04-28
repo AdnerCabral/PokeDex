c42f21a5c2e13d3941bbbb41a0b652404021b002
BT_C09_16 <mobileappdev@bths.edu>
2026-04-28 13:36:27 -0400

    Added data type

---
+++ PokeDex/PokemonInfo.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonInfo.swift
+//  PokeDex
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+struct PokemonInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonInfo()
+}
