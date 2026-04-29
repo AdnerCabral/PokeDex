c0753626ac0ff798cf6a119bd95282d12e9e903d
BT_C09_16 <mobileappdev@bths.edu>
2026-04-29 13:50:20 -0400

    textfield

--- PokeDex/MainWindow.swift
+++ PokeDex/MainWindow.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Pokemon!")
+            HStack {
+                TextField("Enter Pokèmon Dex Num", text: $text)
+                    .padding(.horizontal, 40)
+                    .background(Color(#colorLiteral(red: 0.9294475317, green: 0.9239223003, blue: 0.9336946607, alpha: 1)))
+                    .clipped()
+                    .cornerRadius(10)
+                    .overlay(
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading, 16)
+                        }
+                    )
+                Spacer()
+            }.padding().padding(.top, 30)
         }
-        .padding()
     }
 }
 
