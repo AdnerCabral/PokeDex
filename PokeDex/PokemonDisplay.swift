bd11d975c2370597b2fcb0ac201390b555de622e
BT_C09_01 <mobileappdev@bths.edu>
2026-04-30 13:34:50 -0400

    PokemonDisplay

---
+++ PokeDex/PokemonDisplay.swift
@@ -0,0 +1,32 @@
+//
+//  PokemonDisplay.swift
+//  PokeDex
+//
+//  Created by Student on 4/30/26.
+//
+import SwiftUI
+
+struct PokemonDisplay: View {
+    
+    var id: Int
+    var name: String
+    var height: Int
+    var weight: Int
+  /*   var sprite: [MTLType]
+    var forms: [MTLType]
+    var abilities: [MTLType]
+    var stats: [MTLType]
+    var types: [MTLType]
+    var moves: [MTLType]
+    var species: [MTLType] */
+    
+    var body: some View {
+        HStack{
+            
+        }
+    }
+}
+
+#Preview {
+    PokemonDisplay(id: 67, name: "67", height: 67, weight: 67)
+}
