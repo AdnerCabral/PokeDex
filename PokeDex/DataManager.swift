413a1d71504642fd902146c29cc58d257648f1f8
BT_C09_16 <mobileappdev@bths.edu>
2026-05-04 14:00:21 -0400

    added list

--- PokeDex/DataManager.swift
+++ PokeDex/DataManager.swift
@@ -11,7 +11,9 @@ import Combine
 // Changed from struct to class so it can hold and update data
 @Observable
 class DataManager: ObservableObject {
-    @Published var pokemon: PokemonInfo?
+    
+    private var pokemon: [PokemonInfo] = []
+    private(set) var searchResults: [PokemonInfo] = []
     
     func fetchPokemon(id: String) async {
         let urlStr: String = "https://pokeapi.co\(id.lowercased())"
@@ -24,8 +26,11 @@ class DataManager: ObservableObject {
             // 2. Start the network request
             let (data, _) = try await URLSession.shared.data(from: urlUnwrapped)
             // 3. Decode the JSON into your PokemonInfo struct
-            let decodedResponse = try JSONDecoder().decode(PokemonInfo.self, from: data) {
-            //(data type needed ^^^) 4. Update the UI on the main thread
+            let decodedResponse: PokeResponse = try JSONDecoder().decode(PokeResponse.self, from: data) {
+            // 4. Update the UI on the main thread
+                for mon in decodedResponse.results {
+                    pokemon.append(mon)
+                }
             }
         } catch let error {
             print(error)
