b3f7dc758f879dff08243f934abc3b93d8fbccd6
BT_C09_16 <mobileappdev@bths.edu>
2026-05-04 13:44:21 -0400

    updated data manager

--- PokeDex/DataManager.swift
+++ PokeDex/DataManager.swift
@@ -9,29 +9,27 @@ import SwiftUI
 import Combine
 
 // Changed from struct to class so it can hold and update data
+@Observable
 class DataManager: ObservableObject {
     @Published var pokemon: PokemonInfo?
     
-    func fetchPokemon(id: String) {
+    func fetchPokemon(id: String) async {
+        let urlStr: String = "https://pokeapi.co\(id.lowercased())"
         // 1. Setup the URL
-        guard let url = URL(string: "https://pokeapi.co\(id.lowercased())") else {
-            print("Invalid URL")
+        let url: URL? = URL(string: urlStr)
+        guard let urlUnwrapped = url else {
             return
         }
-        
-        // 2. Start the network request
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                // 3. Decode the JSON into your PokemonInfo struct
-                let decoder = JSONDecoder()
-                if let decodedResponse = try? decoder.decode(PokemonInfo.self, from: data) {
-                    // 4. Update the UI on the main thread
-                    DispatchQueue.main.async {
-                        self.pokemon = decodedResponse
-                    }
-                }
+        do {
+            // 2. Start the network request
+            let (data, _) = try await URLSession.shared.data(from: urlUnwrapped)
+            // 3. Decode the JSON into your PokemonInfo struct
+            let decodedResponse = try JSONDecoder().decode(PokemonInfo.self, from: data) {
+            //(data type needed ^^^) 4. Update the UI on the main thread
             }
-        }.resume()
+        } catch let error {
+            print(error)
+        }
     }
 }
 
