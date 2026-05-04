04202c39f23c2ac32e5c6914a2557ddab50617f8
BT_C09_01 <mobileappdev@bths.edu>
2026-05-04 14:01:59 -0400

    Continued Work on PoekDisplay

--- PokeDex/PokemonDisplay.swift
+++ PokeDex/PokemonDisplay.swift
@@ -25,41 +25,44 @@ struct PokemonDisplay: View {
             ZStack{
                 Color(.darkGray)
                     .ignoresSafeArea()
-                VStack(alignment: .center, spacing: 40)
+                VStack(alignment: .center, spacing: 20)
                 {
                     
-                    
-                    VStack(alignment: .leading)
+                    VStack(alignment: .center)
                             {
                                 Spacer()
-                        
                                 Text(name)
-                                    //.minimumScaleFactor(CGFloat: 30)
-                                    .fontWidth(.expanded)
+                                    .font(.system(size: 40))
+                                    
+                                Text("The (species) pokemon")
+                                    .font(.system(size: 20))
+                                
                                 Text("No. #\(dexid)")
-                                    .font(.subheadline)
+                                    .font(.system(size: 30))
                                     .fontWidth(.expanded)
                                 Spacer()
                             }
-                            .frame(width: 340)
-                            .border(.black)
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 30))
-                            
-                    
-                    .frame(width: 300, height: 20)
-                    Rectangle()
+                            .frame(width: 330, height: 40)
+                Spacer()
+                Rectangle()
+                    .frame(width: 290, height: 6)
+                Spacer()
+                Rectangle()
                         .frame(width: 290, height: 6)
-                ZStack{
+                ZStack
+                    {
                     Color(.lightGray)
                         .ignoresSafeArea()
                     Text("Image Go Here")
-                }
-                .frame(width: 280, height: 280)
-                .background()
-                .border(.black)
+                    }
+                    .frame(width: 280, height: 280)
+                    .background()
+                    .border(.black)
+                    
             }
+                .frame(width: 1, height: 1)
         }
-        .frame(width: 350, height: 500)
+        .frame(width: 350, height: 550)
         .background()
         .border(.black)
         
@@ -67,5 +70,6 @@ struct PokemonDisplay: View {
 }
 
 #Preview {
-    PokemonDisplay(dexid: 67, name: "Galarian Darmanitan (Zen Form)", height: 67, weight: 67)
+    //PokemonDisplay(dexid: 67, name: "Galarian Darmanitan (Zen Form)", height: 67, weight: 67)
+    PokemonDisplay(dexid: 67, name: "Mew", height: 67, weight: 67)
 }
